ccdfa3c228a812570c4b0a89f600bafc512b4b41
Igor <sala-igor@wp.pl>
2024-12-13 14:39:19 +0100

    Dodanie dwoch prostych selectów. Usuniecie niepotrzebnych plikow

--- sql/selects/Selects.sql
+++ sql/selects/Selects.sql
@@ -2,6 +2,28 @@
 set search_path to proj;
 set datestyle to european;
 
+
+-- podstawowe dane pacjentow aby ulatwic wprowadzanie ich do wizyt
+select pacjent_id, imie, nazwisko, nr_telefonu, ulica 
+				from pacjent
+			where nazwisko like 'Kow%';
+		
+		
+-- podstawowe informacje na temat lekarzy
+select pracownik_id, imie, nazwisko, nr_telefonu, o.oddzial_id, o.nazwa as oddzial 
+	from pracownik p
+		join lekarz l on p.pracownik_id = l.lekarz_id
+		join oddzial o USING(oddzial_id)
+	where o.nazwa like 'Neu%';
+	
+-- podstawowe informacje na temat pielegniarzy
+select pracownik_id, imie, nazwisko, nr_telefonu, o.oddzial_id, o.nazwa as oddzial 
+	from pracownik p
+		join pielegniarz l on p.pracownik_id = l.pielegniarz_id
+		join oddzial o USING(oddzial_id)
+	where o.nazwa like 'Neu%';
+
+
 -- wyswietlanie dostepnych rodzajow wizyt dla oddzialu oraz lekarzow ktorych mozemy do nich przypisac
 -- potencjalnie ma za zadanie ulatwic przypisywanie lekarza do wizyty
 select  rw.rodzaj_wizyty_id, rw.opis, string_agg(CONCAT('id:',p.pracownik_id,' ', p.imie,' ', p.nazwisko),', ') as dostepni_pracownicy 
@@ -36,11 +58,12 @@ select p.pracownik_id ,p.imie, p.nazwisko, d.data, (case when d.zmiana = 'R' the
 
 
 -- wyswietlenie wizyt dla lekarza w danym dniu 
-select rw.opis as wizyta, w.data as data, w.godzina as godzina, p.pracownik_id, p.nazwisko from wizyta w 
-			join rodzaj_wizyty rw USING(rodzaj_wizyty_id)
-			join lekarz l USING(lekarz_id) 
-			join pracownik p on p.pracownik_id = l.lekarz_id
-		where w.data = '20-01-2025' and p.nazwisko like 'Zieli%' ;
+select rw.opis as wizyta, w.data as data, w.godzina as godzina, p.pracownik_id, p.nazwisko 
+						from wizyta w 
+							join rodzaj_wizyty rw USING(rodzaj_wizyty_id)
+							join lekarz l USING(lekarz_id) 
+							join pracownik p on p.pracownik_id = l.lekarz_id
+						where w.data = '20-01-2025' and p.nazwisko like 'Zieli%' ;
 
 ---------------------------------------------------------------
 -- wyswietlenie salda dla kazdego pacjenta ktory zalega z platnosciami
@@ -49,19 +72,21 @@ select rw.opis as wizyta, w.data as data, w.godzina as godzina, p.pracownik_id, 
 
 -- zarobki jakie uzyskano w przeciagu ostatniego miesiaca dla kazdego oddzialu
 -- TRZEBA PRZETESTOWAC KIEDY WIECEJ WIZYT BEDZIE
-create view przychody_dla_oddzialow as
-select o.oddzial_id, o.nazwa, SUM(rw.cena) as przychody from oddzial o 
+create view przychody_dla_oddzialow_view as
+select o.oddzial_id, o.nazwa, SUM(rw.cena) as przychody 
+					from oddzial o 
 						join rodzaj_wizyty rw USING(oddzial_id)
 						join wizyta w USING(rodzaj_wizyty_id)
 					group by o.oddzial_id, o.nazwa;
 					
-select * from przychody_dla_oddzialow;
+select * from przychody_dla_oddzialow_view;
 
 
 -- sprawdzenie statystyk ile wizyt mial poszczegolny lekarz w danym miesiacu roku
-select p.imie, p.nazwisko, COUNT(*) as ilosc_wizyt from lekarz l
-		join pracownik p on l.lekarz_id = p.pracownik_id
-		join wizyta w USING(lekarz_id)
-	where extract(month from w.data) = 1  and extract(YEAR from w.data) = 2025-- sprawdzenie dla stycznia 2025
-		group by p.imie, p.nazwisko 
-	order by ilosc_wizyt;
+select p.imie, p.nazwisko, COUNT(*) as ilosc_wizyt 
+					from lekarz l
+						join pracownik p on l.lekarz_id = p.pracownik_id
+						join wizyta w USING(lekarz_id)
+					where extract(month from w.data) = 1  and extract(YEAR from w.data) = 2025-- sprawdzenie dla stycznia 2025
+						group by p.imie, p.nazwisko 
+					order by ilosc_wizyt;
